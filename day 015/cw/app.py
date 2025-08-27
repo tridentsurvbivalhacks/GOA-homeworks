1069463297f8ac22593404bce967e59955a7d1ca
tridentsurvbivalhacks <miqelashvilisando@gmail.com>
2025-08-27 20:39:25 +0400

    second commit

---
+++ day 015/cw/app.py
@@ -0,0 +1,11 @@
+
+#cw #1
+for i in range(10, 55 , 5):
+    print(i)
+
+
+#cw #2
+i = 0
+while i < 50:
+    print("Sandro")
+    i = i + 1
---
+++ day 015/hw/app.py
@@ -0,0 +1,34 @@
+#1) დღევანდელი ნასწავლი მასალა ახსენით კომენტარები
+
+#dges viswavlet while loopebi
+i = 0
+while i < 2:
+    print("While loop")
+    i = i + 1
+#da viswavlet new data list
+                #1     #2     #3      #4       #5
+name_list = ["giorgi, dato, sandro, barbare, natali"]
+print (type(name_list))
+#2) while loop ის გამოყენებით გამოიტანეთ 4 იდან 84 ამდე ციფრები 6 ის გამოტოვებით
+for i in range(4 , 86, 6):
+    print(i)
+#3) შექმენით სია და შეიტანეთ შიგნით სხვადასხვა ელემენტები და გამოიტანეთ მე-2 ,მე-4 და მე-6 ელემენტები და ახსენით როგორ გამოგაქვთ
+               #0        #1    #2  #3     #4      #5    #6
+name_list = ["giorgi", "dato", 18, 12, "natali", 12.4, 20.2]
+print(name_list[2])
+print(name_list[4])
+print(name_list[6])
+#4) ლისტს და ცვლადს შორის განსხვავება ახსენით კომენტარებით
+# cvladi inaxavs mnishvnelobas da cvladis shenaxvit marto shegilia 1 mnishvneloba 1 xazshi sheinaxo da erti cvladshi bevri cvladis shenaxva ar shegilia
+name = " sandro" 
+name2 = " Davit"
+print(name)
+print(name2)
+# list ebi arian igive cvladebi magram is gaachniat gansxvavebuli rom cvladebis shenaxva 1 xaszshi anu 1 cvladis gamoyenebit ramden mnishvnelobasac ginda imdens sheinaxav
+#listebshi atvla iwyeba nolidan amito iqneboda ese
+                #0    #1  #2    #3
+name_list = ["sandro", 2, 12, "davit"]
+print(name_list[2])
+print(name_list[1])
+print(name_list[0])
+print(name_list[3])
