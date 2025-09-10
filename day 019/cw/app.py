fa85f36923a7a3e70ff429bd2629ffa34f54ffd6
tridentsurvbivalhacks <miqelashvilisando@gmail.com>
2025-09-10 15:13:22 +0400

    second commit

---
+++ day 019/cw/app.py
@@ -0,0 +1,25 @@
+print("start")
+
+for i in range(51):
+
+ if i % 2 == 0:
+  print("ლუწი")
+  print(i)
+ else:
+  print("კენტი")
+  print(i)
+
+print("end") 
+
+for i in range(5):
+ 
+ names = ["sandro", "davit", "gio", "saba", "andria",] 
+ 
+ print(i)
+ print(names[i])
+ 
+
+
+ 
+
+
---
+++ day 019/hw/app.py
@@ -0,0 +1,47 @@
+#listebi gavnan cvladebs magram cvladebshi marto shegilia sheinaxo 1 mnishvneloba da listebshi ramdensac ginda imdens
+#listebi agretve aris erterti monacemta tipi da is gamoiyeneba rom ufro dalagebulat chamovtvalot nivtebi listshi 
+#da listebshi arian elementebi da mag elementebs itvlian indexebit
+#index       0         1          2        3       4        5       
+#list    
+fruits = ["Apple", "Samsung", "Xiaomi", "lomi", "Ak-47", "RPG-7", ]  #elements
+item3 = fruits[2]
+
+#2) ფორ ლუპის მეშვეობით რიცხვები გამოიტანეთ 1 დან 100 ჩათვლით
+for i in range(100):
+    print(i)
+
+#3) შექმენით 5 ელემენტისგან შემდგარი სია ხოლო შემდეგ დაბეჭდეთ მისი მე-3 და მე-5 მნიშვნელობა
+
+item = fruits[3]
+item1 = fruits[5]
+print(item + item1)
+
+#4) ვინც საკლასო ვერ გააკეთეთ ეგ გავაკეთოთ სახლში (ვინც ვერ გაიგო შეატყობინეთ ლიდერს <3)
+for i in range(1):
+ 
+ names = ["sandro", "davit", "gio", "saba", "andria",] 
+ name = names[0]
+ name1 = names[1]
+ name2 = names[2]
+ name3 = names[3]
+ name4 = names[4]
+ 
+ print(i)
+ print(name)
+ print(i)
+ print(name1)
+ print(i)
+ print(name2)
+ print(i)
+ print(name3)
+ print(i)
+ print(name4)
+
+#5) შექმენით 7 ელემენტიანი რიცხვებისგან შემდგარი სია, თქვენი დავალებაა გადაუაროთ მას ფორ ციკლით და დაბეჭდოთ ისეთი მნიშვნელობები რომლებიც აღემატება 20-ს
+numbers = [5, 12, 25, 30, 18, 45, 7]
+
+for i in range(7):
+ num = numbers[i]
+ if num > 20:
+   print(num)
+   print(i)
